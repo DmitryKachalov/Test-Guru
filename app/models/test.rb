557995ff018c1c43b2bb69c777f12db036db77a0
DmitryKachalov <rubyzza.dev@gmail.com>
2019-10-21 16:32:12 +0200

    add locales validate

--- app/models/test.rb
+++ app/models/test.rb
@@ -6,10 +6,24 @@ class Test < ApplicationRecord
   has_many :users, through: :test_passages
 
   belongs_to :author, class_name: 'User'
+
+  validates :title, presence: true,
+                    uniqueness: true
+  validates :level, numericality: { only_integer: true }, allow_nil: true
+  validate :validate_max_level, on: :create
+
+  scope :easy, ->(level) { where(level: level).order(created_at: :desc) }
+
   def self.sort_by_category(category_title)
     joins(:category)
         .where(categories: { title: category_title })
         .order(title: :desc)
         .pluck(:title)
+  end
+
+  private
+
+  def validate_max_level
+    errors.add(:level) if level.to_i > 10
   end
 end
