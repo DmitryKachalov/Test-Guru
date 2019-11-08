02fbe9754f484b452aeb8272558d070b138e2e55
DmitryKachalov <rubyzza.dev@gmail.com>
2019-11-08 11:12:55 +0100

    Add -initial basic Authentication (password_digest) -sessions (CookieStorage) - SessionsController and SessionsViews -check existance of #current_user id in session -flash[:alert] message to login form - NavBar with #current_user email if #logged_in?

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
+require 'digest/sha1'
+
 class User < ApplicationRecord
+
   has_many :test_passages, dependent: :nullify
   has_many :tests, through: :test_passages
-
   has_many :tests_created, class_name: 'Test', foreign_key: :author_id
 
-  validates :name, :email, :role, presence: true
+  has_secure_password
 
   def by_level(level)
     tests.level(level)
@@ -13,4 +15,5 @@ class User < ApplicationRecord
   def test_passage(test)
     test_passages.order(id: :desc).find_by(test_id: test.id)
   end
+
 end
