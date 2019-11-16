3d0b5d0074f5f9ab1afce9cc6223076092555fa9
DmitryKachalov <rubyzza.dev@gmail.com>
2019-11-17 00:14:28 +0100

    add localization to Admin::Questions (Controller/Views/Helper)

--- app/controllers/admin/questions_controller.rb
+++ app/controllers/admin/questions_controller.rb
@@ -15,7 +15,7 @@ class Admin::QuestionsController < Admin::BaseController
     @question = @test.questions.new(question_params)
 
     if @question.save
-      redirect_to admin_test_path(@test), notice: 'Question was successfully created.'
+      redirect_to admin_test_path(@test), notice: t('.success')
     else
       render :new
     end
@@ -25,7 +25,7 @@ class Admin::QuestionsController < Admin::BaseController
 
   def update
     if @question.update(question_params)
-      redirect_to admin_test_path(@question.test), notice: 'Question was updated.'
+      redirect_to admin_test_path(@question.test), notice: t('.success')
     else
       render :edit
     end
@@ -33,7 +33,7 @@ class Admin::QuestionsController < Admin::BaseController
 
   def destroy
     @question.destroy
-    redirect_to admin_test_path(@question.test), notice: 'Question was deleted.'
+    redirect_to admin_test_path(@question.test), notice: t('.success')
   end
 
   private
@@ -51,7 +51,7 @@ class Admin::QuestionsController < Admin::BaseController
   end
 
   def rescue_with_question_not_found
-    render plain: 'Question was not found'
+    render plain: t('shared.errors.rescue_with_question_not_found')
   end
 
 end
