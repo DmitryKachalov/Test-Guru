2e4fe7495222dc1abf8d83c76e665d68c7605a43
DmitryKachalov <rubyzza.dev@gmail.com>
2019-11-17 23:03:34 +0100

    add Devise localization (with custom settings)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,8 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(user)
-
+    flash[:notice] = t('devise.sessions.signed_in_welcome_message',
+                       user_full_name: current_user.full_name.html_safe)
     if user.admin?
       admin_tests_path
     else
