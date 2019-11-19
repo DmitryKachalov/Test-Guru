2a9b9a2bf89f387b7d9a67904ddeda92d12d78cf
DmitryKachalov <rubyzza.dev@gmail.com>
2019-11-19 18:16:19 +0100

    add AdminGists#index View and Controller

--- config/routes.rb
+++ config/routes.rb
@@ -28,8 +28,12 @@ Rails.application.routes.draw do
     resources :tests do
       resources :questions, shallow: true, except: :index do
         resources :answers, shallow: true, except: :index
+
+
       end
     end
+
+    resources :gists, only: :index
   end
 
 
