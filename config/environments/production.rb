e61d228ac2fb2316b04b5b16fb6ac814a1be05d9
DmitryKachalov <rubyzza.dev@gmail.com>
2019-11-26 22:08:04 +0100

    fix action.mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,8 @@ Rails.application.configure do
       address:                'smtp.sendgrid.net',
       port:                   587,
       user_name:              ENV['SENDGRID_USERNAME'],
-      password:               ENV['SENDGRID_USERNAME'],
+      password:               ENV['SENDGRID_PASSWORD'],
+      domain:                 'heroku.com',
       authentication:         'plain',
       enable_starttls_auto:   true
   }
